bb5d237451565abbdf9f160dc49a9784db448e3d
Derrick Widner <derrick.widner@outlook.com>
2026-09-07 09:31:13 -0500

    base setup of server done, now publishing web page

--- server/index.js
+++ server/index.js
@@ -1,14 +1,39 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
     message: "J Vick API is running",
+  });
+});
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is healthy",
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
   });
 });
 
